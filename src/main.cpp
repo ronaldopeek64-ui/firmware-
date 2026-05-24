1eea5e77c4ddf85a8d055c70f71bae4e89bc5ab7
ronaldopeek64-ui <ronaldopeek64@gmail.com>
2026-05-24 21:58:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,24 @@
 TFT_eSPI tft = TFT_eSPI(); 
 
 void setup() {
+  // Включаем подсветку экрана (пин 15 на T-Embed)
   pinMode(15, OUTPUT);
   digitalWrite(15, HIGH);
 
+  // Инициализация дисплея
   tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(1); // Горизонтальная ориентация
+  tft.fillScreen(TFT_BLACK); // Черный фон
+
+  // Вывод текста
   tft.setTextColor(TFT_WHITE, TFT_BLACK); 
   tft.setTextSize(3);
-  // Пишем HELLO (кириллица в стандартных шрифтах Arduino обычно не поддерживается)
+  
+  // Пишем HELLO (стандартные шрифты Arduino не поддерживают кириллицу, 
+  // поэтому английскими буквами надежнее)
   tft.drawString("HELLO!", 60, 45);
 }
 
-void loop() {}
+void loop() {
+  // Ничего не делаем
+}
