587a9c11b8e55aef50d5a70acda6042ddea60993
ronaldopeek64-ui <ronaldopeek64@gmail.com>
2026-05-24 21:59:12 +0300

    Create User_Setup.h

---
+++ User_Setup.h
@@ -0,0 +1,18 @@
+#define ST7789_DRIVER
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 135
+
+// Пины LILYGO T-Embed
+#define TFT_MOSI 5
+#define TFT_SCLK 7
+#define TFT_CS   6
+#define TFT_DC   4
+#define TFT_RST  8
+#define TFT_BL   15
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_GFXFF
+
+#define SPI_FREQUENCY  40000000
